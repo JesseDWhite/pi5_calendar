a11928ed1dadeb8a21d313d51db972963fe4dcbb
Jesse White <jesse.white6@gmail.com>
2026-02-18 15:42:09 -0600

    update how the high and low are shown

--- src/App.jsx
+++ src/App.jsx
@@ -74,24 +74,30 @@ const App = () => {
             hour: "2-digit",
             minute: "2-digit",
             hour12: true,
-          })
+          }),
         );
       }
     };
     getForecast();
 
-    const intervalId = setInterval(() => {
-      getForecast();
-    }, 10 * 60 * 1000); // 10 minutes * 60 seconds * 1000 milliseconds
+    const intervalId = setInterval(
+      () => {
+        getForecast();
+      },
+      10 * 60 * 1000,
+    ); // 10 minutes * 60 seconds * 1000 milliseconds
 
     return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
-    const calendarInterval = setInterval(() => {
-      document.getElementById("calendar-iframe").src =
-        "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&showTitle=0&showTz=0&src=amVzc2Uud2hpdGU2QGdtYWlsLmNvbQ&src=M2VjNjJkMDE2NDhmZmU1YTU2NjMyN2U4YzYwYmMxZmIzN2E2YWRiMDU5MTMwMGQ1NGEyNGM0NGE3NzM0NTlmNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MmM2ZDE3M2QwODM1MjExOWRkZmRmODljN2JjNWVhZTBlNjQyOWZjOTAzOWM0OWNjOWU5Mjk4NTdiNDI2YmExZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23f09300&color=%238e24aa&color=%23a79b8e&color=%23009688";
-    }, 10 * 60 * 1000); // 10 minutes * 60 seconds * 1000 milliseconds
+    const calendarInterval = setInterval(
+      () => {
+        document.getElementById("calendar-iframe").src =
+          "https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&showTitle=0&showTz=0&src=amVzc2Uud2hpdGU2QGdtYWlsLmNvbQ&src=M2VjNjJkMDE2NDhmZmU1YTU2NjMyN2U4YzYwYmMxZmIzN2E2YWRiMDU5MTMwMGQ1NGEyNGM0NGE3NzM0NTlmNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=MmM2ZDE3M2QwODM1MjExOWRkZmRmODljN2JjNWVhZTBlNjQyOWZjOTAzOWM0OWNjOWU5Mjk4NTdiNDI2YmExZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23f09300&color=%238e24aa&color=%23a79b8e&color=%23009688";
+      },
+      10 * 60 * 1000,
+    ); // 10 minutes * 60 seconds * 1000 milliseconds
 
     return () => clearInterval(calendarInterval);
   }, []);
@@ -157,7 +163,7 @@ const App = () => {
                             txt.charAt(0).toUpperCase() +
                             txt.substr(1).toLowerCase()
                           );
-                        }
+                        },
                       )}
                     </strong>
                   </span>
@@ -203,7 +209,7 @@ const App = () => {
                             onClick={() =>
                               openDialog(
                                 day.summary,
-                                daysInWeek[new Date(getDate(idx + 1)).getDay()]
+                                daysInWeek[new Date(getDate(idx + 1)).getDay()],
                               )
                             }
                             src={
@@ -219,10 +225,10 @@ const App = () => {
                         <div className="col-12">
                           <span className="day-temp">
                             {" "}
-                            {day.temp.max.toFixed(0)}° /{" "}
+                            {day.temp.min.toFixed(0)}° ~{" "}
                           </span>
                           <span className="night-temp">
-                            {day.temp.min.toFixed(0)}°
+                            {day.temp.max.toFixed(0)}°
                           </span>
                         </div>
                       </div>
